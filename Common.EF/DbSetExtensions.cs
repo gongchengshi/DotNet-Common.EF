c75b647fa4fe88093d019d9e93ca3824bac03066
Jeremy McLain <gongchengshi@gmail.com>
2016-12-06 11:37:45 -0800

    Initial commit

---
+++ Common.EF/DbSetExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Gongchengshi.Collections.Generic;
+
+namespace Gongchengshi.EF
+{
+    public static class DbSetExtensions
+    {
+       static public T FirstOrAdd<T>(this DbSet<T> @this, Func<T, bool> predicate) where T : class, new()
+       {
+          return @this.FirstOr(predicate,
+                               () =>
+                                  {
+                                     var x = new T();
+                                     @this.Add(x);
+                                     return x;
+                                  });
+       }
+
+       static public T FirstOrAdd<T>(this DbSet<T> @this, Func<T, bool> predicate, Func<T> initializer) where T : class
+       {
+          return @this.FirstOr(predicate,
+                               () =>
+                                  {
+                                     var x = initializer();
+                                     @this.Add(x);
+                                     return x;
+                                  });
+       }
+
+       static public T FirstOrAdd<T>(this DbSet<T> @this, Func<T, bool> predicate, T item) where T : class
+       {
+          return @this.FirstOr(predicate,
+                               () =>
+                                  {
+                                     @this.Add(item);
+                                     return item;
+                                  });
+       }
+
+       static public void IfNotContainsAdd<T>(this DbSet<T> @this, Func<T, bool> predicate, Func<T> initializer) where T : class
+       {
+          if (!@this.Any(predicate))
+          {
+             @this.Add(initializer());
+          }
+       }
+    }
+}
---
+++ Common.EF/DropCreateDatabaseTables.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Transactions;
+
+namespace Gongchengshi.EF
+{
+   public class DropCreateDatabaseTables<TContext> : IDatabaseInitializer<TContext> where TContext : DbContext
+   {
+      public void InitializeDatabase(TContext context)
+      {
+         using (new TransactionScope(TransactionScopeOption.Suppress))
+         {
+            if (!context.Database.Exists())
+            {
+               throw new ApplicationException("No database instance");
+            }
+         }
+
+         // remove all tables
+         context.Database.ExecuteSqlCommand(
+            "EXEC sp_MSforeachtable @command1 = \"DROP TABLE ?\"");
+
+         // create all tables
+         var dbCreationScript = ((IObjectContextAdapter) context).ObjectContext.CreateDatabaseScript();
+         context.Database.ExecuteSqlCommand(dbCreationScript);
+
+         Seed(context);
+         context.SaveChanges();
+      }
+
+      protected virtual void Seed(TContext context)
+      {}
+   }
+}
